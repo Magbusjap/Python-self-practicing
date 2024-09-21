f9690223b325bc61147d215ece3c209f483dfad3
Magbusjap <magbusjap@gmail.com>
2024-09-21 14:50:32 +0500

    less-2

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,14 @@
+# example 1
+# C = 2 + 5
+# print (C)
+# C = 2 - 5
+# print (C)
+# C = 2 * 5
+# print (C)
+# C = 2 / 5
+# print (C)
 
-C = 2 + 5
-print (C)
+# example 1
+# sample = input("Введите число: ")
+# sampleTwo = int(sample) + 2
+# print(f"Ответ: {sampleTwo}")
