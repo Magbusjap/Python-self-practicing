a7ac8f30046c8f3aa9fb3a8d2a953cd0ac41e0b9
Magbusjap <magbusjap@gmail.com>
2024-09-26 22:09:57 +0500

    Enter turtleOne

--- Development interface/turtleOne.py
+++ Development interface/turtleOne.py
@@ -1,5 +1,23 @@
 import turtle
+# import tkinter
+# print(tkinter.TkVersion)
 
 bob = turtle.Turtle()
-print(bob)
+# print(bob)
+
+# bob.fd(100)
+# bob.bk(100) # назад
+# bob.lt(100) # влево
+# bob.rt(100) # вправо
+# bob.pu(100) # перо вверх
+# bob.pd(100) # перо вниз
+
+# вариант 1
+bob.fd(100)
+bob.lt(90)
+bob.fd(100)
+
+
 turtle.mainloop()
+
+
